d8702aebed32de8db2c9e7df731b818d9e139fcf
Swapnil Wahile <swapnilwahile24@gmail.com>
2025-12-11 12:14:01 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-95rb#pay%=&(ar+)v7r2-2w4@2tym9b_d)_6*xqarot(e*o%ed
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
